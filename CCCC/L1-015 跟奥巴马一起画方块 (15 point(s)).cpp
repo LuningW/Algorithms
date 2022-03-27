40dba84cb05f0cceeca52e8c9aa96640115d0b33
Luning Wang <93080608+Luuuun@users.noreply.github.com>
2022-03-27 12:11:38 +0800

    Create L1-015 跟奥巴马一起画方块 (15 point(s)).cpp

---
+++ CCCC/L1-015 跟奥巴马一起画方块 (15 point(s)).cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+float n,m;
+char s[5];
+cin>>n>>s[0];
+ m=n/2;
+ 
+for(int i=0;i<(int)(m+0.5);i++){
+    for(int j=0;j<n;j++){
+    cout<<s[0];}
+    cout<<endl;
+    }
+ 
+return 0;
+}

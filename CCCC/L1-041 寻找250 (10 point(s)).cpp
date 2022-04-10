a9c6073a90721df95c01dd7d2888f2a64b555554
Luning Wang <93080608+Luuuun@users.noreply.github.com>
2022-04-10 17:31:14 +0800

    Create L1-041 寻找250 (10 point(s)).cpp

---
+++ CCCC/L1-041 寻找250 (10 point(s)).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i=1,m;
+    while(cin>>m)
+    {
+        if(m==250)
+        {
+            cout<<i<<endl;
+            return 0;
+        }
+        i++;
+    }
+    return 0;
+}
